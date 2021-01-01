8ccae016844ed43baca2539e53bb3bca307f26ac
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-01-01 14:42:15 +0100

    Update dependencies and snowpack configuration

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,12 +8,14 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
+    ['@snowpack/plugin-webpack', { sourceMap: false }],
   ],
   install: [
     /* ... */
   ],
   installOptions: {
-    /* ... */
+    installTypes: true,
+    polyfillNode: true,
   },
   devOptions: {
     /* ... */
