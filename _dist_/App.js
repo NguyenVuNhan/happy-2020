8639c10b43b1f9c1c0a791e00624c2184210584c
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-01-01 03:32:47 +0100

    Updates

---
+++ _dist_/App.js
@@ -0,0 +1,18 @@
+import React from "../web_modules/react.js";
+import "./App.css.proxy.js";
+import Firework from "./components/Firework.js";
+function App({}) {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "App"
+  }, /* @__PURE__ */ React.createElement("header", {
+    className: "App-header"
+  }, /* @__PURE__ */ React.createElement("p", {
+    className: "spinner"
+  }, "Happy 2020")), /* @__PURE__ */ React.createElement(Firework, {
+    style: {position: "absolute", top: 0, left: 0},
+    width: window.innerWidth,
+    height: window.innerHeight,
+    maxParticles: 500
+  }));
+}
+export default App;
