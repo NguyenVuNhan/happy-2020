2982c79b920424541185e63d1fbc799d29fdfbe4
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-01-01 03:23:50 +0100

    Updates

---
+++ hooks/useInterval.js
@@ -0,0 +1,17 @@
+import {useEffect, useRef} from "../web_modules/react.js";
+const useInterval = (callback, delay) => {
+  const callbackRef = useRef(() => {
+  });
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+  useEffect(() => {
+    if (delay === null)
+      return;
+    const id = setInterval(() => callbackRef.current(), delay);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [delay]);
+};
+export default useInterval;
